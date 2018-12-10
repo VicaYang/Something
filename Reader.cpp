7fcaf08363b56fb58e1bdbb311651088dc855d17
HevilH <han051597@naver.com>
2018-12-10 17:48:38 +0800

    Update Reader.cpp

--- Reader.cpp
+++ Reader.cpp
@@ -1,28 +1,134 @@
-﻿#include "Reader.h"
-std::string Reader::read(const std::string& path) {
-    if (plain_text_suffixes.count(path)) {
-        return readFromPlainText(path);
-    }
-    if (doc_suffixes.count(path)) {
-        return readFromPlainText(path);
-    }
-    if (pdf_suffixes.count(path)) {
-        return readFromPlainText(path);
-    }
-    return "";
+#include "Reader.h"
+
+std::wstring Reader::read(const std::wstring& path) {
+	std::wstring format = identifyFormat(path);
+
+	if (plain_text_suffixes.count(format)) {
+		return readFromPlainText(path);
+	}
+	else if (doc_suffixes.count(format)) {
+		return readFromDoc(path);
+	}
+	else if (xls_suffixes.count(format)) {
+		return readFromPlainText(path);
+	}
+	else if (ppt_suffixes.count(format)) {
+		return readFromPlainText(path);
+	}
+	else if (pdf_suffixes.count(format)) {
+		return readFromPdf(path);
+	}
+	else if (html_suffixes.count(format)) {
+		return readFromHtml(path);
+	}
+	else {
+		return L"";
+	}
 }
-std::string Reader::readFromPlainText(const std::string& path) {
-    return "";
+
+std::wstring Reader::identifyFormat(const std::wstring& path) {
+	for (int i = path.length() - 1; i > 0; i--) {
+		if (path[i] == L'.')
+			return path.substr(i+1);
+	}
+	return L"";
 }
-std::string Reader::readFromDoc(const std::string& path) {
-    return "";
+
+std::wstring Reader::readFromPlainText(const std::wstring& path) {
+	int len, n;
+	wchar_t temp[500] = {0,};
+	std::wstring result = L"";
+	FILE *fp = _wfopen(path.c_str(), L"r+,ccs=UTF-8");
+	while (!feof(fp)) {
+		len = fread(temp, 1, sizeof(temp), fp);
+		temp[len / 2 - 1] = 0;
+		result = result + temp;
+	}
+	fclose(fp);
+	return result;
 }
-std::string Reader::readFromPPt(const std::string& path) {
-    return "";
+
+std::wstring Reader::readFromDoc(const std::wstring& path) {
+	std::wstring cmd = L"doctotext\\doctotext\\doctotext.exe ";
+	cmd = cmd + path + L">temp.txt";
+	_wsystem(cmd.c_str());
+	int len, n;
+	wchar_t temp[500] = { 0, };
+	std::wstring result = L"";
+	FILE *fp = _wfopen(L"temp.txt", L"r+,ccs=UTF-8");
+	while (!feof(fp)) {
+		len = fread(temp, 1, sizeof(temp), fp);
+		temp[len/2 - 1] = 0;
+		result = result + temp;
+	}
+	fclose(fp);
+	return result;
 }
-std::string Reader::readFromXls(const std::string& path) {
-    return "";
+
+std::wstring Reader::readFromPPt(const std::wstring& path) {
+	std::wstring cmd = L"doctotext\\doctotext\\doctotext.exe ";
+	cmd = cmd + path + L">temp.txt";
+	_wsystem(cmd.c_str());
+	int len, n;
+	wchar_t temp[500] = { 0, };
+	std::wstring result = L"";
+	FILE *fp = _wfopen(L"temp.txt", L"r+,ccs=UTF-8");
+	while (!feof(fp)) {
+		len = fread(temp, 1, sizeof(temp), fp);
+		temp[len / 2 - 1] = 0;
+		result = result + temp;
+	}
+	fclose(fp);
+	return result;
 }
-std::string Reader::readFromPdf(const std::string& path) {
-    return "";
+
+std::wstring Reader::readFromXls(const std::wstring& path) {
+	std::wstring cmd = L"doctotext\\doctotext\\doctotext.exe ";
+	cmd = cmd + path + L">temp.txt";
+	_wsystem(cmd.c_str());
+	int len, n;
+	wchar_t temp[500] = { 0, };
+	std::wstring result = L"";
+	FILE *fp = _wfopen(L"temp.txt", L"r+,ccs=UTF-8");
+	while (!feof(fp)) {
+		len = fread(temp, 1, sizeof(temp), fp);
+		temp[len / 2 - 1] = 0;
+		result = result + temp;
+	}
+	fclose(fp);
+	return result;
+}
+
+std::wstring Reader::readFromPdf(const std::wstring& path) {
+	std::wstring cmd = L"xpdf\\bin64\\pdftotext ";
+	cmd = cmd + path + L" temp.txt";
+	_wsystem(cmd.c_str());
+	int len, n;
+	wchar_t temp[500] = { 0, };
+	std::wstring result = L"";
+	FILE *fp = _wfopen(L"temp.txt", L"r+,ccs=UTF-8");
+	while (!feof(fp)) {
+		len = fread(temp, 1, sizeof(temp), fp);
+		temp[len / 2 - 1] = 0;
+		result = result + temp;
+	}
+	fclose(fp);
+	return result;
+}
+
+std::wstring Reader::readFromHtml(const std::wstring& path) {
+	std::wstring cmd = L"doctotext\\doctotext\\doctotext.exe ";
+	cmd = cmd + path + L">temp.txt";
+	_wsystem(cmd.c_str());
+	int len, n;
+	wchar_t temp[500] = { 0, };
+	std::wstring result = L"";
+	FILE *fp = _wfopen(L"temp.txt", L"r+,ccs=UTF-8");
+	while (!feof(fp)) {
+		len = fread(temp, 1, sizeof(temp), fp);
+		temp[len / 2 - 1] = 0;
+		result = result + temp;
+	}
+	fclose(fp);
+	return result;
 }
