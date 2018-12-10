a7d076a1165cbd9dd6620b9fd33f7d95c136c1fc
HevilH <han051597@naver.com>
2018-12-10 17:55:24 +0800

    Update FileIndex.h

--- FileIndex.h
+++ FileIndex.h
@@ -1,17 +1,39 @@
-﻿#pragma once
-#include <set>
+#pragma once
+#pragma comment(lib, "NLPIR.lib")
 #include <string>
-#include <map>
+#include <stdlib.h>
+#include <algorithm>
+#include <windows.h>
+#include <stdio.h>
+#include <unordered_map>
+#include <vector>
+#include "Reader.h"
+#include"NLPIR.h"
+
+
+class FileInfo {
+public:
+	int FileNum;
+	std::wstring FileName;
+	std::wstring FilePath;
+	std::wstring FileContent;
+	FileInfo(int num, const std::wstring& path);
+	std::wstring identifyName(const std::wstring& path);
+	std::vector<std::wstring> words;
+};
+
+class post {
+public:
+	int FileNum;
+	int FreqNum;
+};
+
 class FileIndex {
 public:
-    static FileIndex& Instance();
-    FileIndex() {}
-    FileIndex(FileIndex const&) = delete;
-    void operator=(FileIndex const&) = delete;
-    
-private:
-    void buildIndex(char dist);
-    void parseNTFS();
-    std::set<std::string> files;
-    std::map<std::string, std::string> content;
+	int FileNum;
+	std::vector<FileInfo> Files;
+	std::unordered_map<std::wstring, std::vector<post>> DB;
+	FileIndex();
+	void InsertFile(const std::wstring& path);
+	std::vector<FileInfo> SearchFile(const std::wstring& sentence);
 };
