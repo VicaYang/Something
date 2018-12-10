0f25bf17890b84142fd9773ba1dd37e845086fec
HevilH <han051597@naver.com>
2018-12-10 17:47:41 +0800

    Update Reader.h

--- Reader.h
+++ Reader.h
@@ -1,19 +1,26 @@
-﻿#pragma once
+#pragma once
 #include <string>
 #include <set>
+#include <stdlib.h>
+#include <vector>
+#include <windows.h>
+#include <stdio.h>
+
 class Reader {
 public:
-    std::string read(const std::string& path);
+	std::wstring read(const std::wstring& path);
 private:
-    std::set<std::string> plain_text_suffixes = { "txt", "html", "md", "h", "c", "cpp", "hpp", "java" };
-    std::set<std::string> doc_suffixes = { "doc", "docx" };
-    std::set<std::string> xls_suffixes = { "xls", "xlsx" };
-    std::set<std::string> ppt_suffixes = { "ppt", "pptx" };
-    std::set<std::string> pdf_suffixes = { "pdf" };
-    std::string readFromPlainText(const std::string& path);
-        std::string readFromDoc(const std::string& path);
-        std::string readFromXls(const std::string& path);
-        std::string readFromPPt(const std::string& path);
-        std::string readFromPdf(const std::string& path);
-    // others
+	std::set<std::wstring> plain_text_suffixes = { L"txt", L"html", L"md", L"h", L"c", L"cpp", L"hpp", L"java" };
+	std::set<std::wstring> doc_suffixes = { L"doc", L"docx" };
+	std::set<std::wstring> xls_suffixes = { L"xls", L"xlsx" };
+	std::set<std::wstring> ppt_suffixes = { L"ppt", L"pptx" };
+	std::set<std::wstring> pdf_suffixes = { L"pdf" };
+	std::set<std::wstring> html_suffixes = { L"html" };
+	std::wstring readFromPlainText(const std::wstring& path);
+	std::wstring readFromDoc(const std::wstring& path);
+	std::wstring readFromXls(const std::wstring& path);
+	std::wstring readFromPPt(const std::wstring& path);
+	std::wstring readFromPdf(const std::wstring& path);
+	std::wstring readFromHtml(const std::wstring& path);
+	std::wstring identifyFormat(const std::wstring& path);
 };
