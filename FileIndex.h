a8bce536f87f5722c69b9bf3f70c95fc14819f4b
HevilH <han051597@naver.com>
2018-12-30 09:33:57 +0800

    Update FileIndex.h

--- FileIndex.h
+++ FileIndex.h
@@ -7,9 +7,10 @@
 #include <stdio.h>
 #include <unordered_map>
 #include <vector>
+#include <list>
 #include "Reader.h"
 #include"NLPIR.h"
-
+#include <qprocess.h>
 
 class FileInfo {
 public:
@@ -17,7 +18,7 @@ public:
 	std::wstring FileName;
 	std::wstring FilePath;
 	std::wstring FileContent;
-	FileInfo(int num, const std::wstring& path);
+	FileInfo(int num, const std::wstring& path, QProcess *process);
 	std::wstring identifyName(const std::wstring& path);
 	std::vector<std::wstring> words;
 };
@@ -31,9 +32,11 @@ public:
 class FileIndex {
 public:
 	int FileNum;
-	std::vector<FileInfo> Files;
-	std::unordered_map<std::wstring, std::vector<post>> DB;
-	FileIndex();
+	std::list<FileInfo> Files;
+	std::unordered_map<std::wstring, std::list<post>> DB;
+	QProcess *process;
+	FileIndex(QProcess *mprocess);
 	void InsertFile(const std::wstring& path);
+	void DeleteFile(const std::wstring& path);
 	std::vector<FileInfo> SearchFile(const std::wstring& sentence);
 };
